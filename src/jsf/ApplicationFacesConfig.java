3f4a0051635d3a7aa52a2ba1114f8d0ef9d6ee48
cmartins.ifpa <claudio.martins@ifpa.edu.br>
2020-10-25 20:14:38 -0300

    first commit

---
+++ src/jsf/ApplicationFacesConfig.java
@@ -0,0 +1,8 @@
+package jsf;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class ApplicationFacesConfig {
+}
